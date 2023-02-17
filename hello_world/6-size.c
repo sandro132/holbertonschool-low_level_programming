fb9452f07d97eb8f44fdeaa5ac925d9d8f7f04e8
sandro132 <mianveo@hotmail.com>
2023-02-17 09:39:51 -0800

    szs

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,12 +9,12 @@ int main(void)
 {
 char caracter;
 int entero;
-long int longEntero;
+long int longEnter;
 long long int llEnt;
 float flotante;
 printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(caracter));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(entero));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(longEntero));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(longEnter));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(llEnt));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(flotante));
 return (0);
