fc8771895fe0ca2264482b3e5836e624c9fd44ff
sandro132 <mianveo@hotmail.com>
2023-02-17 08:55:05 -0800

    seir

---
+++ hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -a C program
+ *
+ * Return: 0 (Succes);
+ */
+
+int main(void)
+{
+char caracter;
+int entero;
+long int longEntero;
+long long int llEnt;
+float flotante;
+printf("Size of a char: %lu bytes(s)\n2", (unsigned long)sizeof(caracter));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(entero));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(longEntero));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(llEnt));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(flotante));
+return (0);
+}
