185a2af0f58205d9e6ebb30c6c8a81097a131607
sandro132 <mianveo@hotmail.com>
2023-02-21 13:47:12 -0800

    putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <ctype.h>
+/*more  headers goes there*/
+/*betty style doc for function maingoes there*/
+/**
+ * main - function
+ * description -
+ *Return:Always 0 (Success)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
