fbcbb27bf5b2baefe72d766ed0169bb857920517
sandro132 <mianveo@hotmail.com>
2023-02-21 08:44:41 -0800

    alphabet witch for an putchar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - function
+ * description - alphabet
+ * return: 0 (success);
+ */
+
+int main(void)
+{
+	char abc = 'a';
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	putchar(abc);
+	putchar('\n');
+	return (0);
+}
