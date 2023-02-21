c7de54dcce72fdc97754a719f30dda5443161e96
sandro132 <mianveo@hotmail.com>
2023-02-21 08:15:39 -0800

    mayor que menor que

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	printf("Last digit of %d is %d and is greater than 5\n", n, numb);
 	else
 		if (numb == 0)
-		printf("Las digit of %d is %d and is 0\n", n, numb);
+		printf("Last digit of %d is %d and is 0\n", n, numb);
 		else if (n < 6 && numb != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, numb);
 	return (0);
