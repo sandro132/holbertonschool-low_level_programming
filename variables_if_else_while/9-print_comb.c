262444b3a613e2170f1d293b731e1834cbcb8486
sandro132 <mianveo@hotmail.com>
2023-02-21 12:16:13 -0800

    sas

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,8 +12,10 @@ int main(void)
 	for (n = 0; n < 10; n++)
 	putchar(n + '0');
 	if (n < 9)
-	putchar(',');
-	putchar(' ');
+	{
+		putchar(',');
+		putchar(' ');
+	}
 	putchar('\n');
 	return (0);
 }
