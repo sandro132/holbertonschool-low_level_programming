fded5c146ff006325cb3d3c0e775138104cee8bb
sandro132 <mianveo@hotmail.com>
2023-02-21 09:16:37 -0800

    AlPhBeT

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-#include <stdlin.h>
+#include <stdlib.h>
 /**
  * main - function
  * description - alphabet
- * return: 0 (success);
+ * Return: 0 (success);
  */
 
 int main(void)
 {
 	char abc, ABC;
 
-        for (abc = 'a'; abc <= 'z'; abc++)
-	for (ABC = 'A'; ABC <= 'Z': ABC++)
-        putchar(abc);
+	for (abc = 'a'; abc <= 'z'; abc++)
+	for (ABC = 'A'; ABC <= 'Z'; ABC++)
+	putchar(abc);
 	putchar(ABC);
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
