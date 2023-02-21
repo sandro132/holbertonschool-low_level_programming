8b47485988d077935e083e08f03ccc4275f6d832
sandro132 <mianveo@hotmail.com>
2023-02-21 10:43:26 -0800

    hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - function
+ * description -  if for positive or negative
+ *Return: 0 (Success);
+ */
+
+int main(void)
+{
+	int n;
+	char af;
+
+	for (n = 0; n <= 9; n++)
+	putchar(n);
+	for (af = 'a'; af <= 'f'; af++)
+	putchar(af);
+	putchar('\n');
+	return (0);
+}
