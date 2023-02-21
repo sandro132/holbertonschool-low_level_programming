f467c09c2cd65e34a33dd946469fa68e55c5c823
sandro132 <mianveo@hotmail.com>
2023-02-21 12:04:07 -0800

    sas

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - function
+ * description - combination
+ * *Return: 0 (Success);
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	putchar(n + '0');
+	if (n < 9)
+	putchar(',');
+	putchar(' ');
+	putchar('\n');
+	return (0);
+}
