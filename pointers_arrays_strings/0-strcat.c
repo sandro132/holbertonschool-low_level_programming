d8c05a2a789e31a3cf4595455b8bb86380745862
sandro132 <mianveo@hotmail.com>
2023-03-02 08:01:02 -0800

    sum duplicate

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: pointer string
+ * @src: pointer string
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; dest[j] != '\0'; j++)
+	{
+	}
+	printf("%s %s", dest, src);
+
+	printf("%s", src);
+	return (0);
+}
