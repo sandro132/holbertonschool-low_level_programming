1077009a45c2a297588b630487c174420ade86de
sandro132 <mianveo@hotmail.com>
2023-03-02 08:17:32 -0800

    sum duplicate

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -11,13 +11,13 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
-	for (j = 0; dest[j] != '\0'; j++)
+	for (j = 0; src[j] != '\0'; j++)
 	{
+	dest[i] = src[j];
+	i++;
 	}
-	dest[j] = src[i];
-	j++;
 	return (dest);
 }
