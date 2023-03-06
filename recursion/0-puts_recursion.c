15d5e376772c1487cd12c061b74743b38929c1b5
sandro132 <mianveo@hotmail.com>
2023-03-06 09:25:11 -0800

    recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function that prints a string
+ * @s: pointer string
+ * Return 0
+ */
+void _puts_recursion(char *s)
+{
+	printf("%s\n", s);
+}
