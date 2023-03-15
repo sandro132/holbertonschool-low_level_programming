31920e6ccd94badda6685ed0dcae79c6978bf9c3
sandro132 <mianveo@hotmail.com>
2023-03-15 09:04:40 -0700

    concatenates

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,32 +11,28 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *c;
-	unsigned int i, f;
-	int s;
+	int i, f, s, o;
 
-	if (*s1 == '\0' && *s2 == '\0')
+	if (*s1 != '\0' && *s2 != '\0')
 	{
 		for (i = 0; s1[i] != '\0'; i++)
 			;
 		for (f = 0; s2[f] != '\0'; f++)
 		;
 	}
-	c = malloc((i + f) + 1 * sizeof(char));
+	c = malloc((i + f + 1) * sizeof(char));
 
-	if (c == NULL)
+	if (c != NULL)
 	{
-		for (s = 0; c[s] != '\0'; s++)
-		{
-			if (c[s] == '\0')
-
-				c[s] = ' ';
+		for (s = 0; s <= i - 1; s++)
 
 			c[s] = s1[s];
+
+		for (o = 0; o <= f; o++)
+		{
+			c[i] = s2[o];
+			i++;
 		}
-
-		for (s = 0; c[s] != '\0'; s++)
-
-			c[s] = s2[s];
 	}
 	return (c);
 }
