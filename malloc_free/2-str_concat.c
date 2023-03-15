d9ea1580bda7eede51a3837b029b99e9ad65cd81
sandro132 <mianveo@hotmail.com>
2023-03-15 09:50:32 -0700

    null

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,11 +13,11 @@ char *str_concat(char *s1, char *s2)
 	char *c;
 	int i, f, s, o;
 
-	if (*s1 == '\0')
+	if (s1 == NULL)
 
 		s1 = "";
 
-	if (*s2 == '\0')
+	if (s2 == NULL)
 
 		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
