df76eb83f9b6f53dc38437037685455982d909ab
sandro132 <mianveo@hotmail.com>
2023-03-22 13:56:32 -0700

    printf name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,11 +3,15 @@
 
 /**
  * print_name - function that prints a name
+ * @name: pointer to name
  * @f: pointer
  * Return: a pointer
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL)
+	if (name == NULL && f == NULL)
+	{
 		return;
+	}
+	f(name);
 }
