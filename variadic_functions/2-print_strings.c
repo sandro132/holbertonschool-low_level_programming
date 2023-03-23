238c77b4db1dac1cd6931b18d84d111cbca01493
sandro132 <mianveo@hotmail.com>
2023-03-23 14:40:34 -0700

    esde

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -2,21 +2,32 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
- * print_numbers - prints numbers
- * @separator: character to use to delimit
- * @n: number of elements to print
+ * print_strings - print a list of strings
+ * @separator: characters used to delimit
+ * @n: nmber of items to print
  */
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 unsigned int k;
+char *cadena;
 va_list parametros;
 
 	va_start(parametros, n);
 	for (k = 0; k < n; k++)
 	{
-		printf("%d", va_arg(parametros, int));
+		cadena = va_arg(parametros, char *);
+		if (cadena)
+		{
+			printf("%s", cadena);
+		}
+		else
+		{
+			printf("(nil)");
+		}
 		if (separator != NULL && k < n - 1)
-		printf("%s", separator);
+		{
+			printf("%s", separator);
+		}
 	}
 	printf("\n");
 	va_end(parametros);
