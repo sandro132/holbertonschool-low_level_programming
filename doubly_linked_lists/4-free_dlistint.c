71223f596bc1ac3c413c884c42254a16ccb27331
sandro132 <mianveo@hotmail.com>
2023-04-05 20:19:24 -0700

    the allocation is added within the cycle

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -7,11 +7,12 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *tmp = head->next;
+	dlistint_t *tmp;
 
 	while (tmp !=  NULL)
 	{
-	free(head);
-	head = tmp;
+		tmp = head->next;
+		free(head);
+		head = tmp;
 	}
 }
