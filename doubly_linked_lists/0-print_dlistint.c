1488efeb7d504a6539016318a6f22208efffa476
sandro132 <mianveo@hotmail.com>
2023-04-05 19:23:43 -0700

    print all elements

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - function that prints all elements
+ * @h: the head
+ * Return: a count of the nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
