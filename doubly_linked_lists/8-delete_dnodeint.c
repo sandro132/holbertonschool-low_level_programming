bee6a3ce35bc3d5c0f6547ecf692a58ab60e308c
sandro132 <mianveo@hotmail.com>
2023-04-06 09:38:24 -0700

    asda

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -35,7 +35,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	tmp = current->next;
 	if (tmp == NULL)
-		return (-1)
+		return (-1);
 	current->next = temp->next
 	if (current->next != NULL)
 	current->next->prev = current;
