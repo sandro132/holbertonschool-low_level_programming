769c0f6ef4b2711404e91da8192d1baa54a8abe5
sandro132 <mianveo@hotmail.com>
2023-04-06 12:22:21 -0700

    convert a binary numbers

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function that converts a binary number
+ *  @b: pointer string
+ *  Return: 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+
+		result <<=1;
+		result += (*b - '0');
+		b++;
+	}
+
+	return (result);
+}
