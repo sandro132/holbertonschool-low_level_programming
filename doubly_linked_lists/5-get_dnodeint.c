2ff2e4e22a866e95e07684656578e02559990a28
sandro132 <mianveo@hotmail.com>
2023-04-06 08:51:33 -0700

    add decrement

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -8,11 +8,13 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+
 	while (index != 0)
 	{
 		if (head == NULL)
 			return (NULL);
 		head = head->next;
+		index--;
 	}
 
 	return (head);
