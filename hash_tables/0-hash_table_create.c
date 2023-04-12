774968c26395d89db366bfc2ea20b82801762433
sandro132 <mianveo@hotmail.com>
2023-04-12 14:51:46 -0700

    hash table

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function that creates a hash table
+ * @size: size of the array
+ * Return: pointer to the new hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *nht;
+	unsigned long int i;
+
+	nht = malloc(sizeof(hash_table_t));
+	if (nht == NULL)
+		return (NULL);
+
+	nht->size = size;
+	nht->array = malloc(sizeof(hash_table_t *) * size);
+	if (nht->array == NULL)
+		return (NULL);
+
+	while (i < size)
+		ht->array[i] = NULL;
+
+	return (nht);
+}
---
+++ hash_tables/README.md
@@ -0,0 +1,1 @@
+project C-Hash tables
