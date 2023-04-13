645728349cb650e1a63600b95d6bb285f612fef5
sandro132 <mianveo@hotmail.com>
2023-04-13 08:17:48 -0700

    szs

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -9,9 +9,8 @@
  * Return: Always 0
  */
 
-int main(int argc, char *argv[])
+int  main(int argc, char *argv[])
 {
-	if (argc > 0)
-		printf("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
